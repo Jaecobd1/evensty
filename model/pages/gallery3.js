2d1b06c08ea7e498bfd8777d0c5cef0324c8b9d2
Jaecobd1 <jaecob.d1@gmail.com>
2022-10-15 04:54:25 -0400

    added all gallery pages

---
+++ model/pages/gallery3.js
@@ -0,0 +1,16 @@
+var gallery3Content = `<div class="gallery">
+            <div class="hero-image">
+                <div class="image">
+                    <img src="./images/gallery/3.jpg" alt="">
+                </div>
+                <div class="overlay darkGreen-overlay">
+                    <h1 class="number darkGreen-text">3.</h1>
+                    <h1 class="text darkGreen-text">speech</h1>
+                </div>
+            </div>
+            <div class="paragraph">
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem
+                    quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. </p>
+                <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
+            </div>
+        </div>`
---
+++ model/pages/gallery4.js
@@ -0,0 +1,16 @@
+var gallery4Content = `<div class="gallery">
+            <div class="hero-image">
+                <div class="image">
+                    <img src="./images/gallery/4.jpg" alt="">
+                </div>
+                <div class="overlay purple-overlay">
+                    <h1 class="number purple-text">4.</h1>
+                    <h1 class="text purple-text">open foodfest</h1>
+                </div>
+            </div>
+            <div class="paragraph">
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem
+                    quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. </p>
+                <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
+            </div>
+        </div>`
