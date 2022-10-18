7e09f617755ee6bcd22837caa6a9f29c7ca128c0
Jaecobd1 <jaecob.d1@gmail.com>
2022-10-17 20:49:12 -0400

    fixed PWA for school link

--- app/app.js
+++ app/app.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
         navigator.serviceWorker
-            .register("/serviceWorker.js")
+            .register("/~jdobler/final-project/serviceWorker.js")
             .then(res => console.log("service worker is Registered"))
             .catch(err => console.log("service worker not registered err:", err))
 
